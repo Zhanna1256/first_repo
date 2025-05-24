32a1b74c098275aedec9c65103209d8974ceceb8
Zhanna1256 <j.butorina12318@yandex.ru>
2025-05-24 14:18:31 +0500

    lesson_3 commit

--- lesson_1/practice.py
+++ lesson_1/practice.py
@@ -1,5 +1,57 @@
-x = 7.5
-x = int(x)
-y = 10
-result = x + y
-print(result)
+# x = 7.5
+# x = int(x)
+# y = 10
+# result = x + y
+# print(result)
+
+
+# 1
+
+# number_1 = int(input("Введите первое целое число: "))
+# number_2 = int(input("Введите второе целое число: "))
+
+# sum = (number_1 + number_2)
+# print(sum)
+
+
+# # 2
+# str_1 = input("Введите строку: ")
+# result = int(str_1)
+# print(result)
+
+# # 3
+
+# number_3 = int(input("Введите первое целое число: "))
+# number_4 = int(input("Введите второе целое число: "))
+
+# result_2 = number_3 - number_4
+# print(result_2)
+
+# # 4
+# number_5 = float(input("Введите первое дробное число: "))
+# number_6 = float(("Введите второе дробное число: "))
+
+# result_3 = number_5 * number_6
+# print(result_3)
+
+# result_32 = number_5 / number_6
+# print(result_32)
+
+# # 5
+# number_7 = int(input("Введите число: "))
+# print(str(number_7))
+
+# # 6
+# str_2 = str(input("Введите первую строку: "))
+# str_3 = str(input("Введите вторую строку: "))
+
+# result_4 = (str_2 + " " + str_3)
+# print(result_4)
+
+# # 7
+# str_4 = str(input("Введите строку: "))
+# number_8 = int(input("Введите число: "))
+# result_5 = int(str_4 + number_8)
+
+# print(result_5)
+
