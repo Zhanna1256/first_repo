1bc94d3cf5ae000f63a63c01f44f9bd17c775648
Zhanna1256 <j.butorina12318@yandex.ru>
2025-06-05 20:19:30 +0500

    Homework_2

---
+++ Git_intro/lesson_3/homewort_2.py
@@ -0,0 +1,75 @@
+# 1
+
+# summa = 0
+# for i in range(1, 101):
+#     summa += i
+#     print(f"Эх опять прибавляют {summa} " )
+# print(f"Ладно, вот тебе сумма : {summa}")
+
+#2
+
+# line = input("Введите строку ")
+# for i in line:
+#     print(i, "тык-тык")
+
+
+# 3 
+
+# numb_1 = int(input("Введите число "))
+# for i in range(1,11):
+#     result = numb_1 * i
+#     print(numb_1, "*", i, "=" ,result)  
+#     if result % 5 ==0 :
+#         print("Фу, 5 опять...")
+
+#4
+# result = 0 
+# numb_1 = int(input("Введите число "))
+# for i in range(1, numb_1 +1):
+#     if i % 2 ==0:
+#         print(i)
+#         result += 1
+# print(result)
+
+# 5
+
+# numb_1 = int(input("Введите число "))
+# for i in range(1, numb_1 + 1):
+#     if i %3  == 0:
+#         continue
+#     print(i)
+
+# 6
+# summa = 0
+# while True: 
+#     number = int(input("Введите число "))
+#     if number == 0:
+#         break
+#     summa += number
+# print("Сумма введенных чисел: ", summa)
+
+
+
+# 7
+
+# n = int(input("Введите число "))
+# if n %2==0: 
+#     print(" о, ты выбрал четное число ")
+# else:
+#     print("Ха, а ты весёлый. ")
+
+
+# 8
+
+# str_1 = input("Введите строку: ")
+# str_2 = str_1.lower().count("a")
+# print(f"Колличество букв `а`: {str_2}")
+
+
+# 9
+
+# n = int(input("Введите число "))
+# if n %7 ==0:
+#     print("Семёрка... ты наш человек!")
+# else:
+#     print("Хм, подозрительно...")
